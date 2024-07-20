d50a459bba22c2730b26cb939d9b553733e17743
PetriJF <jamespetri28@gmail.com>
2024-07-20 20:04:04 +0100

    initial commit

---
+++ launch/go2_rviz.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Define paths
+    go2_description_share = get_package_share_directory('go2_description')
+    urdf_file = os.path.join(go2_description_share, 'urdf', 'go2_description.urdf')
+    rviz_config_file = os.path.join(go2_description_share, 'config', 'check_joint.rviz')
+
+    # Read the URDF file
+    with open(urdf_file, 'r') as infp:
+        robot_description_content = infp.read()
+
+    return LaunchDescription([
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            parameters=[{'use_gui': True}]
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_description_content}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_file]
+        )
+    ])
